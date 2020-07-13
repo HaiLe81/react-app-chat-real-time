3dbb3dc21b6fee78fe3ba3ccf880b0b98c6dc48d
@KhacHai <lekhachai9999@gmail.com>
2020-07-13 20:39:26 +0700

    add config socket

--- src/configs/globals.js
+++ src/configs/globals.js
@@ -1,7 +1,8 @@
 const env = {
     SERVER_URL: process.env.REACT_APP_SERVER_URL || 'http://localhost:3300',
     API_URL: process.env.REACT_APP_API_URL || 'http://localhost:3300/api/v1',
-    COOKIE_KEY: process.env.REACT_APP_COOKIE_KEY || 'acr-local'
+    COOKIE_KEY: process.env.REACT_APP_COOKIE_KEY || 'acr-local',
+    SOCKET: process.env.REACT_APP_SOCKET || "http://localhost:3300"
 
 }
 
